f11fd2c7e6c5610d7c3592517036e4be9b740266
anonymous-mint <annonymuskmint@gmail.com>
2023-03-26 00:56:00 +0530

    Solitude

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Home from "./components/Home/Home";
 
 function App() {
-  return <div className="App">Love</div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,18 @@
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div className="solitude">
+      <span className="text">S</span>
+      <span className="text">O</span>
+      <span className="text">L</span>
+      <span className="text">I</span>
+      <span className="text">T</span>
+      <span className="text">U</span>
+      <span className="text">D</span>
+      <span className="text">E</span>
+    </div>
+  );
+};
+
+export default Home;
